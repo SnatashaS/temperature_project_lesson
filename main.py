340a4ccc7dc54be669808357c86825b1989f62ed
SnatashaS <natashaserdukova@gmail.com>
2026-04-29 21:08:44 +0300

    задание 1

--- main.py
+++ main.py
@@ -8,14 +8,26 @@ from random import randint
 from input_output import input_lst_temperature, output_temperature_lst
 
 
+# Найти индекс первого дня, когда температура превысила заданное значение.
+def search_index_day_gt(lst_temp):
 
+    num = int(input("Введите заданное значение: "))
 
-
+    for i in range(len(lst_temp)):
+        if lst_temp[i] > num:
+            return i 
 
 
 
 def main():
     lst_temp = []
 
-
     input_lst_temperature(lst_temp)
+
+    output_temperature_lst(lst_temp)
+
+    print("Задание №1")
+    index_gt = search_index_day_gt(lst_temp)
+    print(f"Индекс первого дня, когда температура превысила заданное значение: {index_gt}")
+
+main()
