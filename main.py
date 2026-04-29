73e67618cd08e28e037decb0ad6b803967622e37
SnatashaS <natashaserdukova@gmail.com>
2026-04-29 21:25:52 +0300

    вынесены функции

--- main.py
+++ main.py
@@ -3,19 +3,11 @@
 # Ваша задача - реализовать несколько функций, которые выполняют следующие операции 
 # с этим списком температур:
 
-
 from random import randint 
 from input_output import input_lst_temperature, output_temperature_lst
+from search_day_temp import search_index_day_gt, search_index_day_lt
 
 
-# Найти индекс первого дня, когда температура превысила заданное значение.
-def search_index_day_gt(lst_temp):
-
-    num = int(input("Введите заданное значение: "))
-
-    for i in range(len(lst_temp)):
-        if lst_temp[i] > num:
-            return i 
 
 
 
@@ -29,5 +21,9 @@ def main():
     print("Задание №1")
     index_gt = search_index_day_gt(lst_temp)
     print(f"Индекс первого дня, когда температура превысила заданное значение: {index_gt}")
+
+    print("Задание №2")
+    index_lt = search_index_day_lt(lst_temp)
+    print(f"Индекс первого дня, когда температура превысила заданное значение: {index_lt}")
 
 main()
